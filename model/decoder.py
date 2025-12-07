141412a11ccb87c62d857a33cf4bc2ac56adcb2c
Johnny <johnny.li.1411@gmail.com>
2025-12-07 16:10:46 +0100

    decoder

--- model/decoder.py
+++ model/decoder.py
@@ -4,56 +4,63 @@ from torch import nn
 from layers import *
 
 
-class EncoderBlock(nn.Module):
+class DecoderBlock(nn.Module):
 
     def __init__(self,
                  dim_emb,
+                 dim_cross,
                  num_heads,
                  dim_ff):
         super().__init__()
         self.dim_emb = dim_emb
         self.num_heads = num_heads
         self.dim_ff = dim_ff
-        # Components of the encoder block
-        self.multihead_attentions = MultiHeadAttention(num_heads, dim_emb)
+        # Components of the decoder block
+        self.masked_multihead_attentions = MultiHeadAttention(num_heads, dim_emb, mask=True)
         self.layer_norm_1 = nn.LayerNorm()
+        self.cross_attentions = CrossAttention(num_heads, dim_emb, dim_cross)
+        self.layer_norm_2 = nn.LayerNorm()
         self.fully_connected = FullyConnected(dim_emb, dim_ff)
-        self.layer_norm_2 = nn.LaayerNorm()
-    
-    def forward(self, x_emb):
+        self.layer_norm_3 = nn.LayerNorm()
+
+    def forward(self, x_emb, encoder_output):
         _, _, dim_emb = x_emb.shape
         assert dim_emb == self.dim_emb, "Embedding dimension mismatch"
-        output = ResidualConnection(self.multihead_attentions)(x_emb)
-        output = self.layer_norm(output)
+        output = ResidualConnection(self.masked_multihead_attentions)(x_emb)
+        output = self.layer_norm_1(output)
+        output = ResidualConnection(self.cross_attentions)(output, encoder_output)
+        output = self.layer_norm_2(output)
         output = ResidualConnection(self.fully_connected)(output)
-        output = self.layer_norm(output)
+        output = self.layer_norm_3(output)
         return output
+    
 
-
-class Encoder(nn.Module):
+class Decoder(nn.Module):
 
     def __init__(self,
                  vocab_size,
                  dim_emb,
+                 dim_cross,
                  num_heads,
                  dim_ff,
                  num_layers):
         super().__init__()
         self.vocab_size = vocab_size
         self.dim_emb = dim_emb
+        self.dim_cross = dim_cross
         self.num_heads = num_heads
         self.dim_ff = dim_ff
         self.num_layers = num_layers
-        # Components of the encoder
-        self.input_embedding = Embedding(vocab_size, dim_emb)
-        self.encoder_block = EncoderBlock(dim_emb, num_heads, dim_ff)
+        # Components of the decoder
+        self.output_embedding = Embedding(vocab_size, dim_emb)
+        self.decoder_block = DecoderBlock(dim_emb, dim_cross, num_heads, dim_ff)
 
-    def forward(self, x):
+    def forward(self, x, encoder_output):
         batch, length, vocab_size = x.shape
         assert vocab_size == self.vocab_size, "Vocabulary size mismatch"
-        x_emb = self.input_embedding(x)
+        x_emb = self.output_embedding(x)
         position_emb = self.positional_encoding(length, x_emb.shape[-1])
         output = x_emb + position_emb
         for _ in range(self.num_layers):
-            output = self.encoder_block(output)
+            output = self.decoder_block(output, encoder_output)
         return output
--- model/layers.py
+++ model/layers.py
@@ -1,8 +1,36 @@
+import numpy as np
 import torch as th
 from torch import nn
 from torch.nn import functional as F
 import math
   
+
+def positional_encoding(num_pos, dim_emb):
+    positions = np.arange(num_pos)[:, np.newaxis]
+    dims = np.arange(dim_emb)[np.new_axis, :]
+    i = dims // 2
+    angles = positions * (1 / np.power(10000, 2 * i / dim_emb))
+    angles[:, 0::2] = np.sin(angles[:, 0::2])
+    angles[:, 1::2] = np.cos(angles[:, 1::2])
+    pos_encoding = th.tensor(angles, dtype=th.float32).unsqueeze(0)
+    return pos_encoding
+
+
+class Embedding(nn.Module):
+
+    def __init__(self,
+                 vocab_size,
+                 dim_emb):
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.dim_emb = dim_emb
+        self.embedding = nn.Embedding(vocab_size, dim_emb)
+
+    def forward(self, x):
+        _, _, vocab_size = x.shape
+        assert vocab_size == self.vocab_size, "Vocabulary size mismatch"
+        return self.embedding(x) * math.sqrt(self.dim_emb)
+
 
 class MultiHeadAttention(nn.Module):
 
@@ -12,28 +40,58 @@ class MultiHeadAttention(nn.Module):
         super().__init__()
         self.num_heads = num_heads
         self.dim_head = dim_emb
-        try:
-            assert dim_emb % num_heads == 0
-            self.dim_head = dim_emb // num_heads
-        except AssertionError:
-            raise ValueError("Embedding dimension must be divisible by number of heads")
+        assert dim_emb % num_heads == 0, "Embedding dimension must be divisible by number of heads"
+        self.dim_head = dim_emb // num_heads
+        # Layers for linear projections
         self.input_layer = nn.Linear(dim_emb, 3*dim_emb)
         self.output_layer = nn.Linear(dim_emb, dim_emb)
 
-    def forward(self, x, mask=None):
-        batch, length, dim_emb = x.shape
-        qkv = nn.Linear(dim_emb, 3* dim_emb)(x)
-        qkv = qkv.view(batch, self.num_heads, length, 3*self.dim_head)
+    def forward(self, x_emb, mask=False):
+        batch, length, dim_emb = x_emb.shape
+        assert dim_emb == self.dim_head, "Embedding dimension mismatch"
+        input_shape = batch, self.num_heads, length, 3*self.dim_head
+        qkv = self.input_layer(x_emb).view(input_shape)
         q, k, v = th.chunk(qkv, 3, dim=-1)
         scores = th.matmul(q, k.transpose(-1, -2) / math.sqrt(self.dim_head))
-        if mask is not None:
+        if mask:
             mask = th.ones_like(scores, dtype=th.bool).tril(1)
             scores = scores.masked_fill(mask, float('-inf'))
         weights = F.softmax(scores, dim=-1)
         attention = th.matmul(weights, v).transpose(1,2).reshape(batch, length, dim_emb)
-        output = nn.Linear(dim_emb, dim_emb)(attention)
+        output = self.output_layer(attention)
         return output
     
+
+class CrossAttention(nn.Module):
+
+    def __init__(self,
+                 num_heads,
+                 dim_emb,
+                 dim_cross):
+         super().__init__()
+         self.num_heads = num_heads
+         self.dim_emb = dim_emb
+         assert dim_emb % num_heads == 0, "Embedding dimension must be divisible by number of heads"
+         self.dim_head = dim_emb // num_heads
+         # Layers for linear projections
+         self.query_layer = nn.Linear(dim_emb, dim_emb)
+         self.key_layer = nn.Linear(dim_cross, dim_emb)
+         self.value_layer = nn.Linear(dim_cross, dim_emb)
+         self.output_layer = nn.Linear(dim_emb, dim_emb)
+
+    def forward(self, x_emb, encoder_output):
+        batch, length, dim_emb = x_emb.shape
+        assert dim_emb == self.dim_emb, "Embedding dimension mismatch"
+        input_shape = batch, self.num_heads, length, self.dim_head
+        q = self.query_layer(x_emb).view(input_shape)
+        k = self.key_layer(encoder_output).view(input_shape)
+        v = self.value_layer(encoder_output).view(input_shape)
+        scores = th.matmul(q, k.transpose(-1, -2) / math.sqrt(self.dim_head))
+        weights = F.softmax(scores, dim=-1)
+        attention = th.matmul(weights, v).transpose(1,2).reshape(batch, length, dim_emb)
+        output = self.output_layer(attention)
+        return output
+
 
 class FullyConnected(nn.Module):
 
@@ -43,15 +101,18 @@ class FullyConnected(nn.Module):
         super().__init__()
         self.dim_output = dim_emb
         self.dim_ff = dim_ff
-        self.in_layer(
+        # Layers of the feed-forward network
+        self.in_layer = nn.Sequential([
             nn.Linear(dim_emb, dim_ff),
             nn.RELU(),
             nn.Dropout()
-        )
+            ])
         self.out_layer = nn.Linear(dim_ff, dim_emb)
     
-    def forward(self, x):
-        return self.out_layer(self.in_layer(x))
+    def forward(self, x_emb):
+        _, _, dim_emb = x_emb.shape
+        assert dim_emb == self.dim_output, "Embedding dimension mismatch"
+        return self.out_layer(self.in_layer(x_emb))
 
 
 class ResidualConnection(nn.Module):
